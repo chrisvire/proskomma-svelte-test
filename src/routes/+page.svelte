b990dcd29ffc0ce229094fb4a43e22b55691c3c7
FyreByrd <aejones4gm@gmail.com>
2022-10-19 13:30:24 -0400

    Proskomma Hello World

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+    import {Proskomma} from 'proskomma';
+
+    const pk = new Proskomma();
+    const promise = pk.gqlQuery("{ id }");
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await promise}
+    <p>loading</p>
+{:then data} 
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+{/await}
