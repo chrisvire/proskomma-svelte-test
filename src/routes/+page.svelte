8f549e0762c069750f107798614a90715e8eafd1
FyreByrd <aejones4gm@gmail.com>
2022-10-19 15:15:51 -0400

    Included proskomma-freeze and data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script>
     import {Proskomma} from 'proskomma';
+    import {thaw} from 'proskomma-freeze';
 
     const pk = new Proskomma();
     const promise = pk.gqlQuery("{ id }");
+    const loaded = (async () => {
+        console.log("test");
+
+        const res = await fetch("eng_web.pkf").then((r) => {
+            return r.text();
+        });
+
+        if(res.length) {
+            await thaw(pk, res);
+        }
+        //console.log(res);
+        return await pk.gqlQuery("{docSets { id }}");
+    })();
 </script>
 
 
@@ -10,6 +24,12 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 {#await promise}
+    <p>loading</p>
+{:then data} 
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+{/await}
+
+{#await loaded}
     <p>loading</p>
 {:then data} 
     <pre>{JSON.stringify(data, null, 2)}</pre>
